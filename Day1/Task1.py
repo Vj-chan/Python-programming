14e4246b8d68f620599abf3132e82e389b8ec61d
Vijay chandran Murugesan <100556350+Vj-chan@users.noreply.github.com>
2022-03-07 23:45:25 +0530

    Add files via upload

---
+++ Day1/Task1.py
@@ -0,0 +1,10 @@
+'''
+Created on 02-Mar-2022
+
+@author: ELCOT
+'''
+
+#Program to print a sentence
+
+print("Hi Barani")
+print("This is the program for printing a sentence")
---
+++ Day1/Task3.py
@@ -0,0 +1,12 @@
+'''
+Created on 02-Mar-2022
+
+@author: ELCOT
+'''
+
+#Program to get 2 numbers and print the sum
+
+n1 = float(input("Enter a number \t"))
+n2 = float(input("Enter another number \t"))
+n = n1 + n2
+print("The sum of the numbers is ", n)
